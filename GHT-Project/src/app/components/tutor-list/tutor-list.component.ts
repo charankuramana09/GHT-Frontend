f65bce2b8ad1107ca9770563d392ec67f80caa10
charankuramana09 <charan.kuramana@isigntech.com>
2024-06-12 14:43:41 +0530

    tutor list image updated

--- GHT-Project/src/app/components/tutor-list/tutor-list.component.ts
+++ GHT-Project/src/app/components/tutor-list/tutor-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-tutor-list',
   templateUrl: './tutor-list.component.html',
   styleUrl: './tutor-list.component.css'
 })
-export class TutorListComponent {
+export class TutorListComponent implements OnInit{
 
+
+  imageData: string | ArrayBuffer;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getImageFromBackend();
+  }
+
+  getImageFromBackend() {
+    // Make a GET request to your backend API to fetch the image data
+    this.http.get('http://localhost:5555/api/tutors/register', { responseType: 'blob' })
+      .subscribe((data: Blob) => {
+        // Convert the Blob data to base64 string
+        const reader = new FileReader();
+        reader.readAsDataURL(data);
+        reader.onloadend = () => {
+          this.imageData = reader.result;
+        };
+      });
+  }
 }
